90768d6e8f7245628ddc8e3efb61b26ea0c26c20
antoinetteboyle <antoinetteboyle75@gmail.com>
2022-03-01 10:03:10 +1100

    Bubble plot text feature

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -11,40 +11,97 @@ var svg = d3.select("#scatter")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.csv("../assets/data/data.csv", function(data) {
-   var set = Object.entries(data).forEach(([key, value]) => {
+d3.csv("./assets/data/data.csv").then( function(data) {
+   Object.entries(data).forEach(([key, value]) => {
    console.log(key,value)});
+
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([0, 100])
-    .range([ 0, width ]);
+    .domain([28, 47])
+    .range([ 0, width]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, 100000])
+    .domain([30000, 82000])
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
+
+  // Add a scale for bubble size
+  const z = d3.scaleLinear()
+  .domain([1000, 100000])
+  .range([ 1000, 100000]);
+
+  // Add a scale for bubble color
+  const myColor = d3.scaleOrdinal()
+    .domain(["AL", "AK", "AR", "CA", "CO", "CT"])
+    .range(d3.schemeSet2);
+
+   // -1- Create a tooltip div that is hidden by default:
+   const tooltip = d3.select("#scatter")
+   .append("div")
+     .style("opacity", 0)
+     .attr("class", "tooltip")
+     .style("background-color", "black")
+     .style("border-radius", "5px")
+     .style("padding", "10px")
+     .style("color", "white")
+
+ // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
+ const showTooltip = function(event, d) {
+   tooltip
+     .transition()
+     .duration(200)
+   tooltip
+     .style("opacity", 1)
+     .html("State: " + d.state + ", Income: " + d.income + ", Age: " + d.age + ", Obesity: " + d.obesity)
+     .style("left", (event.x)/2 + "px")
+     .style("top", (event.y)/2+30 + "px")
+ }
+ const moveTooltip = function(event, d) {
+   tooltip
+     .style("left", (event.x)/2 + "px")
+     .style("top", (event.y)/2+30 + "px")
+ }
+ const hideTooltip = function(event, d) {
+   tooltip
+     .transition()
+     .duration(200)
+     .style("opacity", 0)
+ }
+
+ //const label = node.append('text')
+   // .attr('dy', 2)
+   // .text(d => d.poverty.substring(0, d.r / 3));
 
   // Add dots
   svg.append('g')
     .selectAll("dot")
     .data(data)
-    .enter()
-    .append("circle")
-      .attr("class", function(d) { return "bubbles " + d.poverty })
-      .attr("cx", function (d) { return x(d.age); } )
-      .attr("cy", function (d) { return y(d.income); } )
-      //.attr("r", function (d) { return z(d.obesity); } )
-      //.style("fill", function (d) { return myColor(d.poverty); } )
-      .attr("r", 2.5)
-      .style("fill", "#69b3a2")
+    .join("circle")
+      .attr("class", "bubbles")
+      .attr("cx", d => x(d.age))
+      .attr("cy", d => y(d.income))
+      .attr("r", d => z(d.obesity))
+      .style("fill", d => myColor(d.abbr))
+      // -3- Trigger the functions
+    .on("mouseover", showTooltip )
+    .on("mousemove", moveTooltip )
+    .on("mouseleave", hideTooltip )
+    
+    d3.selectAll("bubbles")
+      .append("text")
+      .text("text", d => d.state);
 
+    svg.append("text")      // text label for the x axis
+        .attr("x", 265 )
+        .attr("y", 240 )
+        .style("text-anchor", "middle")
+        .text("state");
 })
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # D3-challenge
+Visualisation analysing the health risks facing particular demographics from the U.S. Census Bureau and the Behavioural Risk Factor Surveillance System based on 2014 ACS 1-year estimates. The data set includes data on rates of income, obesity, poverty, etc. by state. MOE stands for "margin of error."
+
+The main code is found in a folder named [D3_data_journalism]("./D3_data_journalism"). The graphic in the [app.js](./assets/js/app.js) file which pulls in the data from [data.csv](./assets/data/data.csv) using the "d3.csv".
+
+I created a scatter plot:
+- between two of the data variables Income, Age, Obesity!( or such as `Healthcare vs. Poverty` or `Smokers vs. Age`)- representing each state with circle elements and depicting state abbreviations in the circles.
+- situated axes and labels to the left and bottom of the chart.
+
+I used `python -m http.server` to run the visualisation. This will host the page at `localhost:8000` in your web browser.
+
+# 1. More Data, More Dynamics
+
+Additional labels in your scatter plot and give them click events so that your users can decide which data to display. Animate the transitions for your circles' locations as well as the range of your axes. Do this for two risk factors for each axis. Or, for an extreme challenge, create three for each axis.
+
+* Hint: Try binding all of the CSV data to your circles. This will let you easily determine their x or y values when you click the labels.
+
+# 2. Incorporate d3-tip
+
+While the ticks on the axes allow us to infer approximate values for each circle, it's impossible to determine the true value without adding another layer of data. Enter tooltips: developers can implement these in their D3 graphics to reveal a specific element's data when the user hovers their cursor over the element. Add tooltips to your circles and display each tooltip with the data that the user has selected. Use the `d3-tip.js` plugin developed by [Justin Palmer](https://github.com/Caged)—we've already included this plugin in your assignment directory.
