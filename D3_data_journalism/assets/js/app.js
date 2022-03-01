e0aa0af6732ac028589ab66e41614a2bc98fd927
antoinetteboyle <antoinetteboyle75@gmail.com>
2022-03-02 10:17:27 +1100

    add responsiveness

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,11 +1,19 @@
 // @TODO: YOUR CODE HERE!
-
+// set the dimensions and margins of the svg
+function makeResponsive() {
+  var svgArea = d3.select("body").select("svg");
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
+  var svgWidth = window.innerWidth;
+  var svgHeight = window.innerHeight;
+  
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = svgWidth - margin.left - margin.right,
+    height = svgHeight - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
+// append the svg object to the body of the page and group element
 var svg = d3.select("#scatter")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -20,8 +28,8 @@ d3.csv("./assets/data/data.csv").then( function(data) {
 
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([28, 47])
-    .range([ 0, width]);
+    .domain([29, 45])
+    .range([ 0, width-120]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
@@ -40,7 +48,7 @@ d3.csv("./assets/data/data.csv").then( function(data) {
 
   // Add a scale for bubble color
   const myColor = d3.scaleOrdinal()
-    .domain(["AL", "AK", "AR", "CA", "CO", "CT"])
+    //.domain(["AL", "AK", "AR", "CA", "CO", "CT"])
     .range(d3.schemeSet2);
 
    // -1- Create a tooltip div that is hidden by default:
@@ -82,26 +90,44 @@ d3.csv("./assets/data/data.csv").then( function(data) {
 
   // Add dots
   svg.append('g')
+    .attr("class", "node_wrapper")
     .selectAll("dot")
     .data(data)
-    .join("circle")
+    .join("g")
+    .attr("class", "bubble_wrapper")
+    .append("circle")
       .attr("class", "bubbles")
       .attr("cx", d => x(d.age))
       .attr("cy", d => y(d.income))
       .attr("r", d => z(d.obesity))
-      .style("fill", d => myColor(d.abbr))
+      .style("fill", d => myColor(d.abbr)) 
       // -3- Trigger the functions
     .on("mouseover", showTooltip )
     .on("mousemove", moveTooltip )
     .on("mouseleave", hideTooltip )
     
-    d3.selectAll("bubbles")
-      .append("text")
-      .text("text", d => d.state);
+  d3.selectAll(".bubble_wrapper")
+    .data(data)
+    .append("text").text(d => d.abbr)
+    .attr("text-anchor", "middle")
+    .attr("x", d => x(d.age))
+    .attr("y", d => y(d.income))
+      
+  //  svg.append("text")      // text label for the x axis
+  //      .attr("x", 365 )
+  //      .attr("y", 240 )
+  //      .style("text-anchor", "middle")
+  //      .text("state");
 
-    svg.append("text")      // text label for the x axis
-        .attr("x", 265 )
-        .attr("y", 240 )
-        .style("text-anchor", "middle")
-        .text("state");
-})
+//brackets to close then function and catch then errors
+}).catch(function(error) {
+  console.log(error);
+});
+//brackets to close responsive function
+}
+
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);
